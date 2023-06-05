651c65f6185bce1a2fe834157ea868a35ee4cb3f
Luis de Benito <luisdebenito7@gmail.com>
2023-06-05 17:53:55 +0200

    updated webapp branch with only the needed items

--- src/font.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-gameOver_font = pygame.font.Font("freesansbold.ttf", 100)
-credit_font = pygame.font.Font("freesansbold.ttf", 16)
-score_font = pygame.font.Font("freesansbold.ttf", 140)
-welcomePage_font = pygame.font.Font("freesansbold.ttf", 100)
-instructions_font = pygame.font.Font("freesansbold.ttf", 20)
-energybar_font = pygame.font.Font("freesansbold.ttf", 20)
