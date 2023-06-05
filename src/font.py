042cb366c449df7316e82fcac57608bb793fd554
Luis de Benito <luisdebenito7@gmail.com>
2023-06-05 21:42:01 +0200

    changed welcome page

--- src/font.py
+++ src/font.py
@@ -3,6 +3,6 @@ import pygame
 gameOver_font = pygame.font.Font("freesansbold.ttf", 100)
 credit_font = pygame.font.Font("freesansbold.ttf", 16)
 score_font = pygame.font.Font("freesansbold.ttf", 140)
-welcomePage_font = pygame.font.Font("freesansbold.ttf", 100)
+welcomePage_font = pygame.font.Font("freesansbold.ttf", 85)
 instructions_font = pygame.font.Font("freesansbold.ttf", 20)
 energybar_font = pygame.font.Font("freesansbold.ttf", 20)
